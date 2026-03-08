1f1a906e9e04ff415e9b990f88b6f973a85bf628
Viral Soni <sviral572@gmail.com>
2026-03-08 13:05:52 +0530

    Updated Admin Page
    - Check if the logged in user is admin or not before accessing the admin page

--- src/app/(admin)/admin/page.tsx
+++ src/app/(admin)/admin/page.tsx
@@ -3,6 +3,8 @@ import DashboardClient from "@/components/admin/ui/dashboard-client";
 import { Metadata } from "next";
 import { SiteBreadcrumb } from "@/components/ui/breadcrumb";
 import { IconReportAnalytics } from "@tabler/icons-react";
+import { currentUser } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Admin | Dashboard",
@@ -10,7 +12,8 @@ export const metadata: Metadata = {
 
 export default async function AdminPage() {
   const stats = await getDashboardAnalytics();
-
+  const loggedInUser = await currentUser();
+  if (!loggedInUser || loggedInUser.role !== "ADMIN") redirect("/");
   return (
     <div className="container mx-auto p-6 max-w-7xl">
       <SiteBreadcrumb
